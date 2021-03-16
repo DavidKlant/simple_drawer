c9f5f6f01c359cffc8f6bcad82240106439dee29
David Klant <david@card-klant.de>
2021-03-16 17:32:49 +0700

    Add library exports

--- simple_drawer/lib/simple_drawer.dart
+++ simple_drawer/lib/simple_drawer.dart
@@ -1,7 +1,5 @@
 library simple_drawer;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'src/Direction.dart';
+export 'src/SimpleDrawer.dart';
+export 'src/WidgetWrappedInAnimation.dart';
